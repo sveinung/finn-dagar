853a37efdb055cd93088dbeb9757376f9230c98d
Sveinung Dalatun <sdalatun@gmail.com>
2015-12-23 12:33:18 +0100

    Bruk groupingBy i staden og fjern masse unødvendig boilerplate

--- src/main/java/no/sveinung/alt1/FinnDagarAlt1.java
+++ src/main/java/no/sveinung/alt1/FinnDagarAlt1.java
@@ -1,38 +1,25 @@
 package no.sveinung.alt1;
 
 import java.time.LocalDate;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
 
 public class FinnDagarAlt1 {
-    private static final int MINIMUM_ANTAL_FOR_Å_BLI_TALT_MED = 5;
+    private static final int MINIMUM_ANTAL_FOREKOMSTAR_FOR_Å_BLI_TALT_MED = 5;
 
     public static List<Integer> finn(List<LocalDate> datoar) {
-        Map<Integer, Integer> forekomstar = finnForekomstarAvDatoar(datoar);
+        Map<Integer, List<LocalDate>> datoarPerDag = datoar.stream()
+                .collect(groupingBy(LocalDate::getDayOfMonth));
 
-        List<Integer> dagar = finnSannsynligeDagar(forekomstar, MINIMUM_ANTAL_FOR_Å_BLI_TALT_MED);
+        List<Integer> dagar = datoarPerDag.entrySet().stream()
+                .filter((datoarForEinDag) ->
+                        datoarForEinDag.getValue().size() >= MINIMUM_ANTAL_FOREKOMSTAR_FOR_Å_BLI_TALT_MED)
+                .map(Map.Entry::getKey)
+                .collect(toList());
 
         return dagar;
-    }
-
-    private static Map<Integer, Integer> finnForekomstarAvDatoar(List<LocalDate> datoar) {
-        Map<Integer, Integer> forekomstar = new HashMap<>();
-
-        for (LocalDate dato : datoar) {
-            int dag = dato.getDayOfMonth();
-            Integer forekomstAvDag = forekomstar.getOrDefault(dag, 0);
-            forekomstar.put(dag, forekomstAvDag + 1);
-        }
-        return forekomstar;
-    }
-
-    private static List<Integer> finnSannsynligeDagar(
-            Map<Integer, Integer> forekomstar,
-            int minimumAntalForÅBliTaltMed) {
-
-        return forekomstar.entrySet().stream()
-                .filter((dagOgAntal) -> dagOgAntal.getValue() >= minimumAntalForÅBliTaltMed)
-                .map(Map.Entry::getKey)
-                .collect(Collectors.toList());
     }
 }
